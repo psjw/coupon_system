467444f394661091b8459d30599a8ac5787fc123
psjw <dlrudtn108@gmail.com>
2025-06-26 15:04:56 +0900

    feat(domain-member): Member 엔티티 생성 및 공통 엔티티 기반 구조 도입

---
+++ src/main/java/com/psjw/coupon/domain/enums/member/LoginType.java
@@ -0,0 +1,5 @@
+package com.psjw.coupon.domain.enums.member;
+
+public enum LoginType {
+    LOCAL, KAKAO, GOOGLE, NAVER
+}
---
+++ src/main/java/com/psjw/coupon/domain/enums/member/MemberStatus.java
@@ -0,0 +1,5 @@
+package com.psjw.coupon.domain.enums.member;
+
+public enum MemberStatus {
+    ACTIVE, WITHDRAWN, SUSPENDED
+}
