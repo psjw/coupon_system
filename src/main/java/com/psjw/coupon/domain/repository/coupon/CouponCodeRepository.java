9f8cb3392d0ae3dcfcbc1942790cb73e07c28092
psjw <dlrudtn108@gmail.com>
2025-06-29 00:28:29 +0900

    feat(domain-coupon): CouponRepository 인터페이스 추가

---
+++ src/main/java/com/psjw/coupon/domain/repository/coupon/CouponCodeRepository.java
@@ -0,0 +1,23 @@
+package com.psjw.coupon.domain.repository.coupon;
+
+import com.psjw.coupon.domain.model.coupon.CouponBatch;
+import com.psjw.coupon.domain.model.coupon.CouponCode;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CouponCodeRepository {
+    Optional<CouponCode> findByCouponCode(String couponCode);
+    Optional<CouponCode> findById(Long couponId);
+
+    List<CouponCode> findAllByCouponBatch(CouponBatch couponBatch);
+
+
+    boolean existsByCouponCodeAndCouponBatch(String couponCode, CouponBatch couponBatch);
+    boolean existsByCouponCode(String couponCode);
+
+    CouponCode save(CouponCode couponCode);
+    void delete(CouponCode couponCode);
+}
