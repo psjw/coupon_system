2ee57c118b8b0cb9195bcc9663d7c05c89a892ee
psjw <dlrudtn108@gmail.com>
2025-07-21 17:28:01 +0900

    feat(repository-coupon): CouponBatchJobRepository save 반환 타입 Optional제거

--- src/main/java/com/psjw/coupon/domain/repository/coupon/CouponBatchJobRepository.java
+++ src/main/java/com/psjw/coupon/domain/repository/coupon/CouponBatchJobRepository.java
@@ -5,7 +5,7 @@ import com.psjw.coupon.domain.model.coupon.CouponBatchJob;
 import java.util.Optional;
 
 public interface CouponBatchJobRepository {
-    Optional<CouponBatchJob> save(CouponBatchJob couponBatchJob);
+    CouponBatchJob save(CouponBatchJob couponBatchJob);
     void delete(CouponBatchJob couponBatchJob);
     Optional<CouponBatchJob> findById(Long jobId);
 }
