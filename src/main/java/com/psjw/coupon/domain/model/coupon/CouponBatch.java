891f2dcb6c5d074c2b9f0f948aaae05dd8aedbc4
psjw <dlrudtn108@gmail.com>
2025-07-30 18:36:02 +0900

    feat(domain/coupon): CouponBatch 정적 팩토리 메서드 of() 추가

--- src/main/java/com/psjw/coupon/domain/model/coupon/CouponBatch.java
+++ src/main/java/com/psjw/coupon/domain/model/coupon/CouponBatch.java
@@ -15,8 +15,11 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.persistence.UniqueConstraint;
+
 import java.time.LocalDateTime;
+
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -63,5 +66,36 @@ public class CouponBatch extends BaseAuditEntity {
     @Column(nullable = false)
     private BatchStatus batchStatus;
 
+    @Builder
+    private CouponBatch(Long id, CouponInventory couponInventory, String batchName, Integer batchNumber, Integer issueCount, IssueType issueType, LocalDateTime validFromAt, LocalDateTime validUntilAt, BatchStatus batchStatus) {
+        this.id = id;
+        this.couponInventory = couponInventory;
+        this.batchName = batchName;
+        this.batchNumber = batchNumber;
+        this.issueCount = issueCount;
+        this.issueType = issueType;
+        this.validFromAt = validFromAt;
+        this.validUntilAt = validUntilAt;
+        this.batchStatus = batchStatus;
+    }
 
+    public static CouponBatch of(CouponInventory couponInventory,
+                                 String batchName,
+                                 Integer batchNumber,
+                                 Integer issueCount,
+                                 IssueType issueType,
+                                 LocalDateTime validFromAt,
+                                 LocalDateTime validUntilAt,
+                                 BatchStatus batchStatus) {
+        return CouponBatch.builder()
+                .couponInventory(couponInventory)
+                .batchName(batchName)
+                .batchNumber(batchNumber)
+                .issueCount(issueCount)
+                .issueType(issueType)
+                .validFromAt(validFromAt)
+                .validUntilAt(validUntilAt)
+                .batchStatus(batchStatus)
+                .build();
+    }
 }
