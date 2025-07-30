2ef7cc002ab242b9a3b9104f1791f1ab354add55
psjw <dlrudtn108@gmail.com>
2025-07-30 19:49:08 +0900

    refactor(domain-repository): CouponInventoryRepository 관련 순환참조 부분 수정

--- src/main/java/com/psjw/coupon/domain/repository/coupon/CouponInventoryRepository.java
+++ src/main/java/com/psjw/coupon/domain/repository/coupon/CouponInventoryRepository.java
@@ -2,10 +2,12 @@ package com.psjw.coupon.domain.repository.coupon;
 
 import com.psjw.coupon.domain.model.coupon.CouponInventory;
 
+import java.util.Optional;
+
 public interface CouponInventoryRepository {
     CouponInventory save(CouponInventory couponInventory);
     void delete(CouponInventory couponInventory);
 
-    CouponInventory findById(Long inventoryId);
+    Optional<CouponInventory> findById(Long inventoryId);
     boolean existsById(Long inventoryId);
 }
--- src/main/java/com/psjw/coupon/infrastructure/persistence/jpa/coupon/JpaCouponBatchJobRepository.java
+++ src/main/java/com/psjw/coupon/infrastructure/persistence/jpa/coupon/JpaCouponBatchJobRepository.java
@@ -2,7 +2,6 @@ package com.psjw.coupon.infrastructure.persistence.jpa.coupon;
 
 import com.psjw.coupon.domain.model.coupon.CouponBatchJob;
 import com.psjw.coupon.domain.repository.coupon.CouponBatchJobRepository;
-import com.psjw.coupon.domain.repository.coupon.CouponBatchRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -11,20 +10,20 @@ import java.util.Optional;
 @Repository
 @RequiredArgsConstructor
 public class JpaCouponBatchJobRepository implements CouponBatchJobRepository {
-    private final CouponBatchJobRepository couponBatchJobRepository;
+    private final CouponBatchJobJpaRepository couponBatchJobJpaRepository;
 
     @Override
     public CouponBatchJob save(CouponBatchJob couponBatchJob) {
-        return couponBatchJobRepository.save(couponBatchJob);
+        return couponBatchJobJpaRepository.save(couponBatchJob);
     }
 
     @Override
     public void delete(CouponBatchJob couponBatchJob) {
-        couponBatchJobRepository.delete(couponBatchJob);
+        couponBatchJobJpaRepository.delete(couponBatchJob);
     }
 
     @Override
     public Optional<CouponBatchJob> findById(Long jobId) {
-        return couponBatchJobRepository.findById(jobId);
+        return couponBatchJobJpaRepository.findById(jobId);
     }
 }
